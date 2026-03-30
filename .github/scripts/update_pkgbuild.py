10ad54a10395fd6afa3e71cc0385729966fe7a69
hypergonial <git@hypergonial.com>
2026-03-30 17:41:41 +0200

    Do not update sha256sums on pkgrel bumps

--- .github/scripts/update_pkgbuild.py
+++ .github/scripts/update_pkgbuild.py
@@ -3,7 +3,9 @@ import json
 import subprocess
 
 
-def update_pkgbuild(package: str, version: str, pkgrel: int):
+def update_versions(package: str, version: str, pkgrel: int):
+    """Update pkgver and pkgrel in the PKGBUILD file of the given package."""
+
     pkgbuild_path = os.path.join(package, "PKGBUILD")
 
     with open(pkgbuild_path, "r") as f:
@@ -28,10 +30,29 @@ def update_pkgbuild(package: str, version: str, pkgrel: int):
 
 
 def run_updpkgsums(package: str):
+    """Run updpkgsums in the given package directory to recalculate the sha256sums array based on the source array."""
+
     res = subprocess.run(["updpkgsums"], cwd=package, capture_output=True, text=True)
 
     if res.returncode != 0:
         raise RuntimeError(f"updpkgsums failed for {package}:\n{res.stderr}")
+
+
+def update_pkgbuild(
+    package: str,
+    curr_version: str,
+    curr_pkgrel: int,
+    new_version: str,
+    new_pkgrel: int,
+):
+    """Update the PKGBUILD file of the given package with the new version and pkgrel,
+    and recalculate the sha256sums if the version has changed."""
+    update_versions(package, new_version, new_pkgrel)
+
+    # Only update SHA sums if the version has changed,
+    # otherwise we might end up with incorrect sums if the source files haven't changed
+    if curr_version != new_version:
+        run_updpkgsums(package)
 
 
 def main():
@@ -41,17 +62,23 @@ def main():
         packages = json.loads(os.environ["PACKAGE_MATRIX"])["include"]
 
         for pkg in packages:
-            update_pkgbuild(pkg["name"], pkg["latest_version"], pkg["desired_pkgrel"])
-            run_updpkgsums(pkg["name"])
+            update_pkgbuild(
+                pkg["name"],
+                pkg["current_version"],
+                pkg["current_pkgrel"],
+                pkg["latest_version"],
+                pkg["desired_pkgrel"],
+            )
         return
 
     # Fallback to single package mode
     package = os.environ["PACKAGE_NAME"]
     version = os.environ["LATEST_VERSION"]
+    curr_version = os.environ["CURRENT_VERSION"]
     pkgrel = int(os.environ["DESIRED_PKGREL"])
+    curr_pkgrel = int(os.environ["CURRENT_PKGREL"])
 
-    update_pkgbuild(package, version, pkgrel)
-    run_updpkgsums(package)
+    update_pkgbuild(package, curr_version, curr_pkgrel, version, pkgrel)
 
 
 if __name__ == "__main__":
